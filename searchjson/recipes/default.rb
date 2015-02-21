474c506e7c0075312c9e0474af3431f7be52d88b
Thai Pham <thaiphv@gmail.com>
2015-02-22 00:20:04 +1100

    fix bug

--- searchjson/recipes/default.rb
+++ searchjson/recipes/default.rb
@@ -1,4 +1,4 @@
-phpserver = search(:node, "layers:php-app").first
+phpserver = search(:node, "layers:opstest").first
 Chef::Log.info("**********The public IP address is: '#{phpserver[:ip]}'**********")
 
 file "/tmp/ip_addresses" do
